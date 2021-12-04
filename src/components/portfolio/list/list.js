1b79875896833a8899496e5037c0c1f0c6b66be6
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2021-12-04 16:58:06 -0300

    add list.css

--- src/components/portfolio/list/list.js
+++ src/components/portfolio/list/list.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import DateString from "./tools/dateString";
 import isFork from "./tools/isFork";
 import { FaClock } from "react-icons/fa";
+import "./list.css";
 
 export default class list extends Component {
   componentDidMount = () => {
@@ -21,21 +22,21 @@ export default class list extends Component {
     const { reposList } = this.state;
     return (
       <div>
-        <ul className="portfolio-ul">
+        <ul className="list-ul">
           {reposList.map((repos) => {
             const createDate = new Date(repos.updated_at);
             return (
               <a key={repos.name} href={repos.html_url}>
-                <li href={repos.html_url} className="portfolio-li">
-                  <h2 className="portfolio-h2">{repos.name}</h2>
-                  <div className="portfolio-dateFork">
-                    <div className="portfolio-clock-date">
-                      <FaClock className="portfolio-clock-icon" />
+                <li href={repos.html_url} className="list-li">
+                  <h2 className="list-h2">{repos.name}</h2>
+                  <div className="list-dateFork">
+                    <div className="list-clock-date">
+                      <FaClock className="list-clock-icon" />
                       {DateString.create(createDate)}
                     </div>
                     {isFork.check(repos.fork)}
                   </div>
-                  <p className="portfolio-p">{repos.description}</p>
+                  <p className="list-p">{repos.description}</p>
                 </li>
               </a>
             );
