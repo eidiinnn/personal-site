05fd4a70518c29e87e735ff1d2024f8dc0dc181e
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2021-12-04 20:18:58 -0300

    make email works

--- src/components/contato/contato.js
+++ src/components/contato/contato.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
+import emailjs from "emailjs-com";
 import "./contato.css";
 
 export default class Contato extends Component {
+  email = (e) => {
+    e.preventDefault();
+    const toSend = {
+      from_name: e.target.from_name.value,
+      message: e.target.message.value,
+    };
+    emailjs
+      .send(
+        process.env.REACT_APP_SERVICE,
+        process.env.REACT_APP_TEMPLATE,
+        toSend,
+        process.env.REACT_APP_USER
+      )
+      .then(
+        (response) => {
+          alert("Enviado");
+        },
+        (err) => {
+          console.log(
+            "Error, entre em contato comigo diretamente em eduardosilva.pn@gmail.com"
+          );
+        }
+      );
+  };
+
   render() {
     return (
       <section id="contato" className="default-section contato-section">
         <h1 className="default-selection-h1 contato-h1">Contato</h1>
-        <form className="contato-form" action="" method="post">
+        <form id="form" className="contato-form" onSubmit={this.email}>
           <label className="contato-label">Email:</label>
-          <input className="contato-input" type="email"></input>
+          <input id="from_name" className="contato-input" type="email"></input>
           <label className="contato-label">Mensagem:</label>
-          <textarea className="contato-input contato-input-text"></textarea>
+          <textarea id="message" className="contato-input contato-input-text"></textarea>
           <input className="contato-submit" type="submit" value="Enviar"></input>
         </form>
       </section>
