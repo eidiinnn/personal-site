3ae102a0ec1103fe5d3213f3019505007f4c7f92
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2021-12-04 21:11:31 -0300

    fix profile picture bug

--- src/components/sobre/sobre.js
+++ src/components/sobre/sobre.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import "./sobre.css";
+import profile from "./profile.jpg";
 
 export default class Sobre extends Component {
   render() {
     return (
       <section id="sobre" className="default-section sobre-section">
         <div className="sobre-container">
-          <img className="sobre-profile-image" src="/profile.jpg" alt="it's me"></img>
+          <img className="sobre-profile-image" src={profile} alt="it's me"></img>
           <h1 className="sobre-h1">Eduardo da Silva</h1>
           <p className="sobre-p">
             Tenho 23 anos e sou apaixonado por tecnologia, gosto muito de aprender coisas
