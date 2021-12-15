505a16cc9cda9d52768734c9f91326077d6bfdbb
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2021-12-15 10:39:22 -0300

    create htmlList

--- src/components/portfolio/repositoriesList/createRepositoriesList.js
+++ src/components/portfolio/repositoriesList/createRepositoriesList.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import JsonRepositoriesListNames from "./repositoriesListNames.json";
-import DateString from "./tools/dateString";
-import IsFork from "./tools/isFork";
-import { FaClock } from "react-icons/fa";
-import "./repositoriesList.css";
+import createHtmlList from "./htmlList/createHtmlList";
 
 export default class CreateRepositoriesList extends Component {
   componentDidMount = () => {
@@ -23,29 +20,6 @@ export default class CreateRepositoriesList extends Component {
   };
   render() {
     const { repositoriesList } = this.state;
-    return (
-      <div>
-        <ul className="list-ul">
-          {repositoriesList.map((repository) => {
-            const createdDate = new Date(repository.created_at);
-            return (
-              <a key={repository.name} href={repository.html_url}>
-                <li href={repository.html_url} className="list-li">
-                  <h2 className="list-h2">{repository.name}</h2>
-                  <div className="list-dateFork">
-                    <div className="list-clock-date">
-                      <FaClock className="list-clock-icon" />
-                      {DateString.formatToString(createdDate)}
-                    </div>
-                    {IsFork.check(repository.fork)}
-                  </div>
-                  <p className="list-p">{repository.description}</p>
-                </li>
-              </a>
-            );
-          })}
-        </ul>
-      </div>
-    );
+    return <>{createHtmlList.create(repositoriesList)};</>;
   }
 }
---
+++ src/components/portfolio/repositoriesList/htmlList/createHtmlList.js
@@ -0,0 +1,31 @@
+import DateString from "./tools/dateString";
+import IsFork from "./tools/isFork";
+import { FaClock } from "react-icons/fa";
+import "./htmlList.css";
+
+export default class createHtmlList {
+  static create(repositoriesList) {
+    return (
+      <ul className="list-ul">
+        {repositoriesList.map((repository) => {
+          const createdDate = new Date(repository.created_at);
+          return (
+            <a key={repository.name} href={repository.html_url}>
+              <li href={repository.html_url} className="list-li">
+                <h2 className="list-h2">{repository.name}</h2>
+                <div className="list-dateFork">
+                  <div className="list-clock-date">
+                    <FaClock className="list-clock-icon" />
+                    {DateString.formatToString(createdDate)}
+                  </div>
+                  {IsFork.check(repository.fork)}
+                </div>
+                <p className="list-p">{repository.description}</p>
+              </li>
+            </a>
+          );
+        })}
+      </ul>
+    );
+  }
+}
