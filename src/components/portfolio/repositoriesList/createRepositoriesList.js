af4e5e827cf8da98e0ff38d2d12c0e3ceec940d1
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-01-04 11:48:36 -0300

    Create test for Github API and refactoring

--- src/components/portfolio/repositoriesList/createRepositoriesList.js
+++ src/components/portfolio/repositoriesList/createRepositoriesList.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
 import JsonRepositoriesListNames from "./repositoriesListNames.json";
+import Github from "./githubAPI/github";
 import createHtmlList from "./htmlList/createHtmlList";
 
 export default class CreateRepositoriesList extends Component {
   componentDidMount = () => {
-    for (const repositoryName of JsonRepositoriesListNames) {
-      fetch(`https://api.github.com/repos/eidiinnn/${repositoryName}`)
-        .then((res) => res.json())
-        .then((repositoryData) => {
-          this.setState((previousState) => ({
-            repositoriesList: [...previousState.repositoriesList, repositoryData],
-          }));
+    Github.createRepositoriesWithDetailsList("eidiinnn", JsonRepositoriesListNames).then(
+      (returnedRepositoriesList) => {
+        this.setState({
+          repositoriesList: returnedRepositoriesList,
+          promiseResolved: true,
         });
-    }
+      }
+    );
   };
 
   state = {
-    repositoriesList: [],
+    repositoriesList: {},
+    promiseResolved: false,
   };
   render() {
-    const { repositoriesList } = this.state;
-    return <>{createHtmlList.create(repositoriesList)};</>;
+    if (!this.state.promiseResolved) return null;
+    return <>{createHtmlList.create(this.state.repositoriesList.list)};</>;
   }
 }
---
+++ src/components/portfolio/repositoriesList/githubAPI/github.js
@@ -0,0 +1,22 @@
+export default class Github {
+  static async createRepositoriesWithDetailsList(userName, RepositoriesNameList) {
+    const toBeReturned = {
+      requisitionsStatus: { allFail: false, failNumber: 0 },
+      list: [],
+    };
+
+    for (const repositoryName of RepositoriesNameList) {
+      await fetch(`https://api.github.com/repos/${userName}/${repositoryName}`)
+        .then((res) => res.json())
+        .then((githubResponse) => {
+          if (!githubResponse.id) toBeReturned.requisitionsStatus.failNumber = +1;
+          else toBeReturned.list.push(githubResponse);
+        });
+    }
+
+    if (toBeReturned.requisitionsStatus.failNumber === RepositoriesNameList.length)
+      toBeReturned.requisitionsStatus.allFail = true;
+
+    return Promise.resolve(toBeReturned);
+  }
+}
