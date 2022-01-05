a770cb4b0f0a3957fcff461bb30c329c9188bca4
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-01-05 15:11:05 -0300

    message error when don't load the repositories

--- src/components/portfolio/repositoriesList/createRepositoriesList.js
+++ src/components/portfolio/repositoriesList/createRepositoriesList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import JsonRepositoriesListNames from "./repositoriesListNames.json";
 import Github from "./githubAPI/github";
 import createHtmlList from "./htmlList/createHtmlList";
+import Error from "./errorMessage/error";
 
 export default class CreateRepositoriesList extends Component {
   componentDidMount = () => {
@@ -19,8 +20,15 @@ export default class CreateRepositoriesList extends Component {
     repositoriesList: {},
     promiseResolved: false,
   };
+
   render() {
     if (!this.state.promiseResolved) return null;
+
+    if (this.state.repositoriesList.requisitionsStatus.allFail === true)
+    return <><Error /></>
+    
+
     return <>{createHtmlList.create(this.state.repositoriesList.list)};</>;
   }
+
 }
