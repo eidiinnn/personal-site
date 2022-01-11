2e6126cce06d03b7c057bf18c12c0d3d6096259f
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-01-11 12:27:26 -0300

    create  textsAndConfigs

--- src/components/portfolio/repositoriesList/createRepositoriesList.js
+++ src/components/portfolio/repositoriesList/createRepositoriesList.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
-import JsonRepositoriesListNames from "./repositoriesListNames.json";
+import { textsAndConfigs } from "../../../textsAndConfigs.js";
 import Github from "./githubAPI/github";
 import createHtmlList from "./htmlList/createHtmlList";
 import Error from "./errorMessage/error";
 
 export default class CreateRepositoriesList extends Component {
   componentDidMount = () => {
-    Github.createRepositoriesWithDetailsList("eidiinnn", JsonRepositoriesListNames).then(
-      (returnedRepositoriesList) => {
-        this.setState({
-          repositoriesList: returnedRepositoriesList,
-          promiseResolved: true,
-        });
-      }
-    );
+    Github.createRepositoriesWithDetailsList(
+      "eidiinnn",
+      textsAndConfigs.repositoriesList
+    ).then((returnedRepositoriesList) => {
+      this.setState({
+        repositoriesList: returnedRepositoriesList,
+        promiseResolved: true,
+      });
+    });
   };
 
   state = {
@@ -25,10 +26,12 @@ export default class CreateRepositoriesList extends Component {
     if (!this.state.promiseResolved) return null;
 
     if (this.state.repositoriesList.requisitionsStatus.allFail === true)
-    return <><Error /></>
-    
+      return (
+        <>
+          <Error />
+        </>
+      );
 
     return <>{createHtmlList.create(this.state.repositoriesList.list)};</>;
   }
-
 }
---
+++ src/textsAndConfigs.js
@@ -0,0 +1,7 @@
+export const textsAndConfigs = {
+  repositoriesList: ["My-pet-picture", "Personal-website"],
+  headerText:
+    "“Medir o progresso da programação por linhas de código é como medir o progresso da construção de aeronaves em termos de peso.” -Bill Gates",
+  sobreMimText:
+    "Tenho 23 anos e sou apaixonado por tecnologia, gosto muito de aprender coisas novas e sempre me disponho a isso, tenho experiência em NodeJS, MongoDB, API, React entre outros.",
+};
