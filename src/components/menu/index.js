ad99e23f1db46d30ab3d5ed14731b9ec6a7acbb8
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-01-26 12:58:07 -0300

    user styled-component in menu

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,26 +1,26 @@
 import React, { Component } from "react";
-import "./menu.css";
+import { NavMenu, NavTitle, NavUl, NavLi, NavLink } from "../../style";
 
 export default class Menu extends Component {
   render() {
     return (
-      <nav id="inicio" className="menu-nav">
-        <h3 className="menu-h3">&lt;Eduardo&nbsp;/&gt;</h3>
-        <ul className="menu-ul">
-          <a href="#inicio">
-            <li className="menu-li menu-li-Selected">/Início/</li>
-          </a>
-          <a href="#sobre">
-            <li className="menu-li">/Sobre/</li>
-          </a>
-          <a href="#portfolio">
-            <li className="menu-li">/Portfólio/</li>
-          </a>
-          <a href="#contato">
-            <li className="menu-li">/Contato/</li>
-          </a>
-        </ul>
-      </nav>
+      <NavMenu>
+        <NavTitle>&lt;Eduardo&nbsp;/&gt;</NavTitle>
+        <NavUl>
+          <NavLi>
+            <NavLink href="#inicio">/Início/</NavLink>
+          </NavLi>
+          <NavLi>
+            <NavLink href="#sobre">/Sobre/</NavLink>
+          </NavLi>
+          <NavLi>
+            <NavLink href="#portfolio">/Portfólio/</NavLink>
+          </NavLi>
+          <NavLi>
+            <NavLink href="#contato">/Contato/</NavLink>
+          </NavLi>
+        </NavUl>
+      </NavMenu>
     );
   }
 }
