829963077ae3e008a4e33ed5f5648b930bf84e51
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-01-26 12:57:44 -0300

    user styled-component in header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,27 +2,31 @@ import React, { Component } from "react";
 import { BiCodeAlt } from "react-icons/bi";
 import { Typewriter } from "typewriting-react";
 import { textsAndConfigs } from "../../textsAndConfigs";
-import "./header.css";
+import {
+  Header,
+  ShortText,
+  HeaderIconTextContainer,
+  HeaderIcon,
+  HeaderRoleParaBaixo,
+} from "../../style";
 
 export default class Menu extends Component {
   render() {
     return (
-      <header className="header-head">
-        <div className="header-container">
-          <BiCodeAlt className="header-container-icon" />
-          <div className="header-container-text">
-            <div className="header-container-html-text">&lt;p&gt;</div>
-            <p className="header-container-text-p">{textsAndConfigs.headerText}</p>
-            <div className="header-container-html-text">&lt;/p&gt;</div>
-          </div>
-        </div>
-        <div className="msg-role-para-baixo">
+      <Header>
+        <HeaderIconTextContainer>
+          <HeaderIcon>
+            <BiCodeAlt />
+          </HeaderIcon>
+          <ShortText>{textsAndConfigs.headerText}</ShortText>
+        </HeaderIconTextContainer>
+        <HeaderRoleParaBaixo>
           <Typewriter
             cursorClassName="cursor"
             words={["(roleParaBaixo) => {}", "<RoleParaBaixo />"]}
           />
-        </div>
-      </header>
+        </HeaderRoleParaBaixo>
+      </Header>
     );
   }
 }
