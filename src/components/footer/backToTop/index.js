be5eb865d4306f6552fcfbcfee952cda08afb917
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-01-26 12:57:25 -0300

    user styled-component in footer

--- src/components/footer/backToTop/index.js
+++ src/components/footer/backToTop/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { FaArrowUp } from "react-icons/fa";
 import "./backToTop.css";
+import { BackToTopCircle } from "../../../style";
 
 export default class BackTopTop extends Component {
   render() {
     return (
       <a href="#inicio">
-        <div className="backToTop-div">
+        <BackToTopCircle>
           <FaArrowUp />
-        </div>
+        </BackToTopCircle>
       </a>
     );
   }
