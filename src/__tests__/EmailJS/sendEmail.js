1374347e34aed810b99215d896f66093a5f70230
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-01-31 18:28:02 -0300

    fix the tests

--- src/__tests__/EmailJS/sendEmail.js
+++ src/__tests__/EmailJS/sendEmail.js
@@ -1,14 +1,14 @@
 import Email from "../../components/contact/sendEmail";
 
 test("when receiving a with an empty string", () => {
-  const emailConst = new Email("", "message");
+  const emailConst = new Email("", "", "message");
   return emailConst.sendEmail().then((returnMessage) => {
     expect(returnMessage).toBe("Não pode haver espaço vazio");
   });
 });
 
 test("all functions work together", () => {
-  const emailConst = new Email("teste@teste.com", "message");
+  const emailConst = new Email("teste", "teste@teste.com", "message");
   return emailConst.sendEmail().then((returnMessage) => {
     expect(returnMessage).toBe("Enviado");
   });
