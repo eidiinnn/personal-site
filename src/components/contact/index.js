c0fa6ca25281c1e901f3660c7165700fbd93bf60
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2022-03-10 16:51:21 -0300

    Contact changes

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import Email from "./sendEmail";
-import { FaSmile } from "react-icons/fa";
+import { FaPaperPlane } from "react-icons/fa";
 import {
   Form,
   Submit,
   InputTextEmail,
   TextArea,
-  LinksContainer,
-  LinksIconSmile,
-  LinksTitle,
-  LinksList,
-  Link,
+  Icon,
 } from "../../style/contact";
 import { Container, CategoryTitle, CategoryText } from "../../style/global";
 
@@ -32,42 +28,27 @@ export default class Contact extends Component {
       <Container id="contact">
         <CategoryTitle>&lt;Contato /&gt;</CategoryTitle>
         <CategoryText>
-          Entre em Contato comigo via Email ou através dos links.
+          Querendo me oferecer um job de Freelancer ou vaga de emprego? Entre em
+          contato comigo e vamos conversar.
         </CategoryText>
 
-        <Container row noMargin spaceBetween>
-          <Form id="form" onSubmit={this.email}>
-            <InputTextEmail
-              id="name"
-              type="text"
-              placeholder="Nome"
-            ></InputTextEmail>
-            <InputTextEmail
-              id="email"
-              type="email"
-              placeholder="Email"
-            ></InputTextEmail>
-            <TextArea id="message" placeholder="Mensagem"></TextArea>
-            <Submit type="submit" value="Enviar"></Submit>
-          </Form>
-
-          <LinksContainer>
-            <LinksIconSmile>
-              <FaSmile />
-            </LinksIconSmile>
-            <LinksTitle>Meus Links</LinksTitle>
-            <LinksList>
-              <Link href="www.linkedin.com/in/eduardosilvapn">
-                /Linkedin/&nbsp;
-              </Link>
-              <Link href="https://api.whatsapp.com/send?phone=5548996303675">
-                /WhatsApp/&nbsp;
-              </Link>
-              <Link href="https://twitter.com/eidiin_">/Twitter/&nbsp;</Link>
-              <Link href="https://github.com/eidiinnn">/Github/</Link>
-            </LinksList>
-          </LinksContainer>
-        </Container>
+        <Form id="form" onSubmit={this.email}>
+          <Icon>
+            <FaPaperPlane />
+          </Icon>
+          <InputTextEmail
+            id="name"
+            type="text"
+            placeholder="Nome"
+          ></InputTextEmail>
+          <InputTextEmail
+            id="email"
+            type="email"
+            placeholder="Email"
+          ></InputTextEmail>
+          <TextArea id="message" placeholder="Mensagem"></TextArea>
+          <Submit type="submit" value="Enviar"></Submit>
+        </Form>
       </Container>
     );
   }
--- src/style/contact.js
+++ src/style/contact.js
@@ -6,27 +6,42 @@ export const Form = styled.form`
   flex-direction: column;
   align-self: left;
   grid-row: 2;
-  width: 500px;
+  width: 600px;
+  align-self: center;
+  justify-content: center;
+  align-items: center;
+  margin-top: 4rem;
+`;
+
+export const Icon = styled.div`
+  font-size: 4rem;
+  color: ${secondaryColor};
+  margin-bottom: 2rem;
 `;
 
 export const InputTextEmail = styled.input`
+  background-color: ${DarkBackgroundColor};
+  color: ${normalTextColor};
+  width: 100%;
   margin: 0 0 1rem 0;
+  padding: 0.5rem 0.2rem 0.5rem 0.2rem;
   height: 25px;
   border: 0;
-  padding: 0.4rem;
 `;
 
 export const TextArea = styled.textarea`
+  background-color: ${DarkBackgroundColor};
+  color: ${normalTextColor};
+  width: 100%;
   margin: 0 0 1rem 0;
+  padding: 0.5rem 0.2rem 0.5rem 0.2rem;
   height: 200px;
-  padding: 0.4rem;
 `;
 
 export const Submit = styled.input`
   width: 100px;
   padding: 0.5rem;
   border: 0;
-  border-radius: 10px;
   align-self: flex-end;
   transition: 1000ms;
   background: ${DarkBackgroundColor};
@@ -39,44 +54,4 @@ export const Submit = styled.input`
     color: ${DarkBackgroundColor};
     background-color: ${secondaryColor};
   }
-`;
-
-export const LinksContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  align-items: center;
-  margin-left: 10rem;
-`;
-
-export const LinksIconSmile = styled.div`
-  width: 80px;
-  height: 80px;
-  border-radius: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${DarkBackgroundColor};
-  color: ${secondaryColor};
-  font-size: 3rem;
-`;
-
-export const LinksTitle = styled.h1`
-  color: ${secondaryColor};
-  font-size: 1.6rem;
-  margin: 0;
-  margin-top: 0.2rem;
-  margin-bottom: 1rem;
-`;
-
-export const LinksList = styled.p`
-  font-size: 1.2rem;
-  font-weight: 700;
-  margin: 0;
-  color: ${normalTextColor};
-  text-decoration: none;
-`;
-
-export const Link = styled.a`
-  color: ${normalTextColor};
 `;
