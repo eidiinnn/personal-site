92a523729e43dc81abd76c88501d83bc3fb28a40
Eduardo da Silva <eduardosilva.pn@gmail.com>
2022-05-04 17:55:40 -0300

    remove the -

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -27,20 +27,20 @@ export default function Skills() {
 
         <SkillsContentContainer>
           <TechNameUl>
-            <TechNameLi color="#EFD81D">Javascript</TechNameLi> -
-            <TechNameLi color="#5ED3F3">React</TechNameLi> -
-            <TechNameLi color="#7248B6">Redux</TechNameLi> -
-            <TechNameLi color="#3594CF">Css</TechNameLi> -
-            <TechNameLi color="#944058">Jest</TechNameLi> -
-            <TechNameLi color="#2391E6">Docker</TechNameLi> -
-            <TechNameLi color="#0AC980">Figma</TechNameLi> -
-            <TechNameLi color="#E84E32">Git</TechNameLi> -
-            <TechNameLi color="#E8E8E8">Linux</TechNameLi> -
-            <TechNameLi color="#43853d">NodeJS</TechNameLi> -
-            <TechNameLi color="#bf225a">Ejs</TechNameLi> -
-            <TechNameLi color="#E8E8E8">Express</TechNameLi> -
-            <TechNameLi color="#5ED3F3">Windows</TechNameLi> -
-            <TechNameLi color="#D80621">Inglês</TechNameLi> -
+            <TechNameLi color="#EFD81D">Javascript</TechNameLi>
+            <TechNameLi color="#5ED3F3">React</TechNameLi>
+            <TechNameLi color="#7248B6">Redux</TechNameLi>
+            <TechNameLi color="#3594CF">Css</TechNameLi>
+            <TechNameLi color="#944058">Jest</TechNameLi>
+            <TechNameLi color="#2391E6">Docker</TechNameLi>
+            <TechNameLi color="#0AC980">Figma</TechNameLi>
+            <TechNameLi color="#E84E32">Git</TechNameLi>
+            <TechNameLi color="#E8E8E8">Linux</TechNameLi>
+            <TechNameLi color="#43853d">NodeJS</TechNameLi>
+            <TechNameLi color="#bf225a">Ejs</TechNameLi>
+            <TechNameLi color="#E8E8E8">Express</TechNameLi>
+            <TechNameLi color="#5ED3F3">Windows</TechNameLi>
+            <TechNameLi color="#D80621">Inglês</TechNameLi>
             <TechNameLi color="#D467AB">Styled-components</TechNameLi>
           </TechNameUl>
 
