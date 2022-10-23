3eb27e5bfaad0c46a92f8fd673d1ca6a57d10ac7
Eduardo da Silva <eduardosilva.pn@gmail.com>
2022-10-23 10:39:17 -0300

    list style refactor

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,25 +1,33 @@
 import react from 'react'
 import { textsAndConfigs } from '../textsAndConfigs'
-import { Container, CategoryTitle, CategoryText } from '../style/global'
-import { ProjectListUl, ProjectListLi, ProjectListTitle, Tech, Text} from '../style/portfolio'
+import {
+  Container,
+  CategoryTitle,
+  CategoryText,
+  ListUl,
+  ListLi,
+  ListTitle,
+  ListShortText,
+  ListLongText,
+} from '../style/global'
 
 export default function Experience() {
   return (
     <Container id='experience'>
       <CategoryTitle>Experiência</CategoryTitle>
       <CategoryText>Minha Experiência como desenvolvedor.</CategoryText>
-      <ProjectListUl>
+      <ListUl>
         {textsAndConfigs.experiences.map((job) => {
           return (
-            <ProjectListLi key={job.name} noHeight>
-              <ProjectListTitle>{job.name}</ProjectListTitle>
-              <Tech primary marginBottom>{`${job.started} - ${job.end}`}</Tech>
-              <Tech>{job.techs}</Tech>
-              <Text>{job.text}</Text>
-            </ProjectListLi>
+            <ListLi key={job.name} noHeight>
+              <ListTitle>{job.name}</ListTitle>
+              <ListShortText primary marginBottom>{`${job.started} - ${job.end}`}</ListShortText>
+              <ListShortText>{job.techs}</ListShortText>
+              <ListLongText>{job.text}</ListLongText>
+            </ListLi>
           )
         })}
-      </ProjectListUl>
+      </ListUl>
     </Container>
   )
 }
--- src/style/global.js
+++ src/style/global.js
@@ -180,3 +180,90 @@ a:active {
   }
 }
 `;
+
+export const ListUl = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  grid-row: 2;
+  justify-content: center;
+
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+
+  @media screen and (max-width: 664px) {
+    width: 100%;
+  }
+`;
+
+export const ListLi = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: ${(props) => (props.center ? "center" : "space-between")};
+
+  width: 400px;
+  height: ${(props) => (props.noHeight ? '' : '230px')};
+  margin: 0.7rem;
+
+  border-top: 4px solid ${secondaryColor};
+  padding: 2.5rem;
+  box-shadow: 5px 5px 7px 5px rgba(0, 0, 0, 0.12);
+  background-color: ${DarkBackgroundColor};
+  transition-duration: 700ms;
+
+  font-family: "Roboto";
+  color: ${normalTextColor};
+
+  &:hover {
+    box-shadow: 0px 0px 52px -6px rgba(50, 47, 74, 1);
+  }
+
+  @media screen and (max-width: 1053px) {
+    width: 30%;
+    height: auto;
+  }
+
+  @media screen and (max-width: 761px) {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+export const ListTitle = styled.h1`
+  font-family: "Bebas Neue", cursive;
+  color: ${primaryColor};
+  align-self: left;
+  font-size: 2rem;
+
+  margin: 0;
+  margin-bottom: 0.1rem;
+`;
+
+export const ListShortText = styled.p`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-weight: 700;
+  margin: 0;
+  margin-bottom: ${(props) => props.marginBottom ? '5px' : 'unset'};
+  font-size: 1.1rem;
+  color: ${(props) => (props.primary ? primaryColor : 'unset')};
+
+  @media screen and (max-width: 1053px) {
+    max-width: 100%;
+  }
+`;
+
+export const ListLongText = styled.p`
+  color: ${normalTextColor};
+  text-align: left;
+  font-size: 1rem;
+  font-weight: 400;
+  width: 380px;
+
+  @media screen and (max-width: 1053px) {
+    width: 100%;
+    height: max-content;
+  }
+`;
